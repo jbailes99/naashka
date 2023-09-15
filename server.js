3659fa89a62473be428bd657f1924f32953a8b02
DQ <contact@dahquan.com>
2023-09-15 00:34:42 -0700

    chore: build app then launch server on deploy

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express')
+const fs = require('fs')
+
 const PORT = process.env.PORT || 5001
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  const query = req.query.root || '.'
+  console.log('query', query.root)
+
+  const list = fs.readdirSync(query)
+  console.log('list', list)
+
+  res.send(`
+  <h1>Query: ${query}</h1>
+  <ul>
+    ${list.map(item => `<li>${item}</li>`).join('')}
+  </ul>`)
 })
 
 app.listen(PORT, () => {
