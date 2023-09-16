17ee81d5b356830d0d91b033187a02dfb44e49bb
DQ <contact@dahquan.com>
2023-09-15 18:55:04 -0700

    chore: remove test script

--- server.js
+++ server.js
@@ -1,25 +1,10 @@
 const express = require('express')
-const fs = require('fs')
 
 const PORT = process.env.PORT || 5001
 const app = express()
 
 // set the public root to the build folder
 app.use(express.static('build'))
-
-app.get('/test', (req, res) => {
-  const query = req.query.root || '.'
-  console.log('query', query.root)
-
-  const list = fs.readdirSync(query)
-  console.log('list', list)
-
-  res.send(`
-  <h1>Query: ${query}</h1>
-  <ul>
-    ${list.map(item => `<li>${item}</li>`).join('')}
-  </ul>`)
-})
 
 app.listen(PORT, () => {
   console.log('The server is up on ' + PORT)
